643b7ff649bddffbe0f604b1cbc086d0aba1f7ee
rata sorin <ratasorin0@gmail.com>
2020-11-21 17:05:25 +0200

    teo

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-var width, height;
+var width, height, width1, height1;
+var lungime = document.querySelector("html");
+var gridParinte = document.querySelector(".patrat-grid");
+let gridCopil = document.querySelector(".patrat");
 window.onresize = window.onload = function () {
   width = this.innerWidth;
+  width1 = width;
   height = this.innerHeight;
-  console.log();
+  height1 = height;
+  console.log("asta e widht " + width);
+  console.log("asta e height " + height);
+  console.log(" ");
+  while (width != height) {
+    if (width > height) width = width - height;
+    else height = height - width;
+  }
+  lungime.style.fontSize = width + "px";
+
+  for (var i = 0; i <= height1; i += width) {
+    var clone = gridCopil.cloneNode(true);
+    console.log(clone);
+    gridParinte.appendChild(clone);
+  }
 };
