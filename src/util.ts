0f8ca02302860e210b1338b60014e7cf1969eef9
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2020-11-26 20:26:15 +0200

    Format document

--- src/util.ts
+++ src/util.ts
@@ -1,16 +1,9 @@
-export const generateArray = <T>(
-  length: number,
-  generatorFunc: (elem: T, index: number, array: T[]) => T
-): T[] => new Array(length).fill(null).map(generatorFunc)
+export const generateArray = <T>(length: number, generatorFunc: (elem: T, index: number, array: T[]) => T): T[] =>
+  new Array(length).fill(null).map(generatorFunc)
 
-export const randInt = (limit: number): number =>
-  Math.floor(Math.random() * limit)
+export const randInt = (limit: number): number => Math.floor(Math.random() * limit)
 
-export const appendElements = <T>(
-  parent: Element,
-  elements: T[],
-  elemRetrievalFunc = (elem: T | Node) => elem as Node
-): void => {
+export const appendElements = <T>(parent: Element, elements: T[], elemRetrievalFunc = (elem: T | Node) => elem as Node): void => {
   elements.forEach((elem) => parent.appendChild(elemRetrievalFunc(elem)))
 }
 
