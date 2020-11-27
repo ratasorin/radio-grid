f756ac9e56a1078a5fee66c194d005c8eeb6f5fb
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2020-11-28 01:28:55 +0200

    Fix bug where multiple grid rebuilds would be triggered, add dark mode support and logging

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,60 @@
-import { Grid } from './components/Grid'
-import { randInt } from './util'
-import { SquareEventListener } from './components/Square'
-
 import './reset.css'
 import './style.css'
 
-const colors = [
+import { Grid } from './components/Grid'
+import { log, randInt } from './util'
+import { SquareEventListener } from './components/Square'
+import { Component } from './internal/Component'
+
+const colorSchemes = [
   ['#fdc5f5', '#f7aef8', '#b388eb', '#8093f1', '#72ddf7'],
   ['#587291', '#2f97c1', '#1ccad8', '#15e6cd', '#0cf574'],
 ]
 
-const gridParent = document.createElement('div')
-gridParent.classList.add('grid__parent')
-document.body.appendChild(gridParent)
+const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
 
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+const gridParentDOM = document.querySelector<HTMLElement>('.grid__parent')
+if (!gridParentDOM) {
+  throw new Error(`Grid parent doesn't exist in the HTML document!`)
+}
+const gridParent = Component.from(gridParentDOM)
 
-const squareMousedown: SquareEventListener = async (square) => {
-  await square.destroy(true)
-  if (randInt(7) === 6) {
-    square.removeEventListener('mousedown')
-    await grid.destroy(true)
-    await grid.appendTo(gridParent, true)
-    square.addEventListener('mousedown', squareMousedown)
+let ignoreClicks = false
+
+const squareMousedown: SquareEventListener<'mousedown'> = async function () {
+  if (ignoreClicks) {
+    return
   }
+  const removed = grid.removeSquare(this, true)
+  log(`${grid.existingSquares.length} squares left`)
+  const rand = randInt(7)
+  log(`Random integer generated: ${rand}`)
+  if (rand === 6) {
+    ignoreClicks = true
+    await removed
+    await grid.destroy(true)
+    await grid.create(gridParent, true)
+  }
+  ignoreClicks = false
 }
 
 const grid = new Grid({
   squareCount: 48,
-  colors: colors[prefersDarkMode ? 0 : 1],
+  colors: colorSchemes[prefersDarkMode.matches ? 0 : 1],
   squareEventListeners: {
     mousedown: squareMousedown,
   },
 })
 
 const main = async () => {
-  await grid.appendTo(gridParent, true)
+  await grid.create(gridParent, true)
+  prefersDarkMode.addEventListener('change', (ev) => {
+    if (ev.matches) {
+      grid.setColors(colorSchemes[0])
+    } else {
+      grid.setColors(colorSchemes[1])
+    }
+  })
 }
 
 main()
