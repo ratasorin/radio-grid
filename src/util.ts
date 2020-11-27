1b697d50980d9c7fb0c75da71908b3230280398b
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2020-11-28 01:29:42 +0200

    Add different utilities and instantiate logger

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,26 @@
+import debug from 'debug'
+
 export const generateArray = <T>(length: number, generatorFunc: (elem: T, index: number, array: T[]) => T): T[] =>
   new Array(length).fill(null).map(generatorFunc)
 
 export const randInt = (limit: number): number => Math.floor(Math.random() * limit)
 
-export const appendElements = <T>(parent: Element, elements: T[], elemRetrievalFunc = (elem: T | Node) => elem as Node): void => {
-  elements.forEach((elem) => parent.appendChild(elemRetrievalFunc(elem)))
+export const isDefined = <T>(arg: T | undefined): arg is T => typeof arg !== 'undefined'
+
+export class UnreachableError extends Error {
+  constructor(x: never, message: string) {
+    super(`TypeScript thought we could never end up here\n${message}`)
+  }
 }
 
-export const GCD = (a: number, b: number): number => {
-  if (b === 0) {
-    return a
-  }
-  return GCD(b, a % b)
+export const wait = (time: number): Promise<void> => new Promise<void>((resolve) => setTimeout(resolve, time))
+
+const log = debug('app:log')
+if (process.env.ENVIRONMENT) {
+  debug.enable('*')
+  log('Logging is enabled!')
+} else {
+  debug.disable()
 }
+
+export { log }
