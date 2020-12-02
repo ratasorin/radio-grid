7c5cf62d636e2c599940898406d62d97e8226576
Teodor Maxim <57960185+tmaxmax@users.noreply.github.com>
2020-12-02 11:15:19 +0200

    Add color variables and remove dark theme

--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,9 @@ import './reset.css'
 import './style.css'
 
 import { Grid } from './components/Grid'
-import { baseLog, isDefined, randInt } from './util'
-import { Square, SquareEventListener } from './components/Square'
+import { randInt } from './util'
+import { Square } from './components/Square'
 import { Component } from './internal/Component'
-
-const log = baseLog.extend('index')
-
-const colorSchemes = [
-  ['#fdc5f5', '#f7aef8', '#b388eb', '#8093f1', '#72ddf7'],
-  ['#587291', '#2f97c1', '#1ccad8', '#15e6cd', '#0cf574'],
-]
-
-const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)')
 
 const gridParentDOM = document.querySelector<HTMLElement>('.grid__parent')
 if (!gridParentDOM) {
@@ -21,48 +12,33 @@ if (!gridParentDOM) {
 }
 const gridParent = Component.from(gridParentDOM)
 
-const squareMousedown: SquareEventListener = {
-  event: 'mousedown',
-  callback: (() => {
-    let ignoreClicks = false
-    // FIXME: Better square removal method
-    return async function (this: Square) {
-      if (ignoreClicks) {
-        return
-      }
-      grid.removeSquare(this).destroy(true)
-      log('%d squares left', grid.squareCount)
-      const rand = randInt(7)
-      log(`Random number generated: ${rand}`)
-      if (rand === 6) {
-        ignoreClicks = true
-        await grid.destroy(true)
-        await grid.create(gridParent, true)
-      }
-      ignoreClicks = false
+const squareMousedown = (() => {
+  let ignoreClicks = false
+  // FIXME: Better square removal method
+  return async function (this: Square) {
+    if (ignoreClicks) {
+      return
     }
-  })(),
-}
+    grid.removeSquare(this).destroy(true)
+    if (grid.squareCount === randInt(grid.squareCount + 1)) {
+      ignoreClicks = true
+      await grid.destroy(true)
+      await grid.create(gridParent, true)
+    }
+    ignoreClicks = false
+  }
+})()
 
 const grid = new Grid({
-  colors: colorSchemes[prefersDarkMode.matches ? 0 : 1],
-  squareEventListeners: [squareMousedown],
+  squareCount: 48,
+  colors: ['cotton-candy', 'mauve', 'lavender-floral', 'cornflower-blue', 'sky-blue'].map((color) => `var(--color-${color})`),
+  squareEventListeners: [
+    {
+      event: 'mousedown',
+      callback: squareMousedown,
+    },
+  ],
 })
-
-function colorSchemeListener(this: MediaQueryList, ev: MediaQueryListEvent): void {
-  if (ev.matches) {
-    grid.setColors(colorSchemes[0])
-  } else {
-    grid.setColors(colorSchemes[1])
-  }
-}
-
-if (isDefined(prefersDarkMode.addEventListener)) {
-  prefersDarkMode.addEventListener('change', colorSchemeListener)
-} else {
-  // for Safari
-  prefersDarkMode.addListener(colorSchemeListener)
-}
 
 const main = async () => {
   await grid.create(gridParent, true)
